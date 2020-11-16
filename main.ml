17211ed36f365b6cf84f6bc71b8b799d46bc2fea
Hao Wu <echowuhao@gmail.com>
2020-11-16 12:14:56 +0800

    js

--- main.ml
+++ main.ml
@@ -1,4 +1,13 @@
-let square x = x * x
-let () = print_endline "Hello World"
-let () = print_endline @@ string_of_int (square 3)
+open Js_of_ocaml
+
+let square_ml x = x * x
+
+let _ =
+  Js.export "myMathLib"
+    (object%js
+       method add x y = x +. y
+       method abs x = abs_float x
+       method square = square_ml
+       val zero = 0.
+   end)
 
